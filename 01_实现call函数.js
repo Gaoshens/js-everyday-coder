ec44b3a9aba920815255136be991a8162fc1c982
gaoshen <1395647813@qq.com>
2022-04-19 23:09:59 +0800

    实现call函数

---
+++ 01_实现call函数.js
@@ -0,0 +1,7 @@
+Function.prototype.call = function (context, ...args) {
+  const fn = Symbol();
+  context[fn] = this;
+  const result = context[fn](...args);
+  delete context[fn]
+  return result;
+}
